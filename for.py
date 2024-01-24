763398c4f2c4b1ff65392748b86a9c34d160d775
Worachai-10 <139629440+Worachai-10@users.noreply.github.com>
2024-01-24 16:13:01 +0700

    Update for.py

--- for.py
+++ for.py
@@ -1,19 +1,19 @@
 re1 = ""
 for i in range(1, 16, 2):
-    result_1 += str(i) + ", "
+    re1 += str(i) + ", "
 print(re1)
 
 re2 = ""
 for i in range(2, 20, 3):
-    result_2 += str(i) + ", "
+    re2 += str(i) + ", "
 print(re2)
 
 re3 = ""
 for i in range(30, -31, -10):
-    result_3 += str(i) + ", "
+    re_3 += str(i) + ", "
 print(re3)
 
 re4 = ""
 for i in range(15, 56, 8):
-    result_4 += str(i) + ", "
+    re_4 += str(i) + ", "
 print(re4)
