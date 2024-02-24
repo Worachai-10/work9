97b4c6e191011c0a86e3cac83de36080df52de7f
Worachai-10 <139629440+Worachai-10@users.noreply.github.com>
2024-02-24 00:39:32 -0800

    Update for.py

--- for.py
+++ for.py
@@ -10,10 +10,10 @@ print(re2)
 
 re3 = ""
 for i in range(30, -31, -10):
-    re_3 += str(i) + ", "
+    re3 += str(i) + ", "
 print(re3)
 
 re4 = ""
 for i in range(15, 56, 8):
-    re_4 += str(i) + ", "
+    re4 += str(i) + ", "
 print(re4)
